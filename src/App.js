1d6df97d099f96f5e521579ee1e82aefa124a2ca
eliseyseverniy <qwelseerty@gmail.com>
2020-12-29 15:23:05 +0300

    end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import React from 'react'
-import TodoList from "./todo/Todolist";
+import React, { useEffect } from 'react'
+import TodoList from './Todo/TodoList'
+import Context from './context'
+import Loader from './Loader'
+import Modal from './Modal/Modal'
+
+const AddTodo = React.lazy(
+  () =>
+    new Promise(resolve => {
+      setTimeout(() => {
+        resolve(import('./Todo/AddTodo'))
+      }, 3000)
+    })
+)
 
 function App() {
-    const todos = [
-        {id: 1, completed: false, title: 'buy bread'},
-        {id: 2, completed: false, title: 'buy eggs'},
-        {id: 3, completed: false, title: 'buy milk'
-        }]
-    function toggleTodo(id){
-        console.log('todois', id)
+  const [todos, setTodos] = React.useState([])
+  const [loading, setLoading] = React.useState(true)
 
-    }
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTimeout(() => {
+          setTodos(todos)
+          setLoading(false)
+        }, 2000)
+      })
+  }, [])
 
-    return (
-        <div className={'wrapper'}>
-            <h1> My React App
-        </h1>
-            <TodoList todos={todos} onToggle={toggleTodo}/>
-        </div>
-    );
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    )
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(
+      todos.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: false
+        }
+      ])
+    )
+  }
+
+  return (
+    <Context.Provider value={{ removeTodo }}>
+      <div className='wrapper'>
+        <h1>React tutorial</h1>
+        <Modal />
+
+        <React.Suspense fallback={<Loader />}>
+          <AddTodo onCreate={addTodo} />
+        </React.Suspense>
+
+        {loading && <Loader />}
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggleTodo} />
+        ) : loading ? null : (
+          <p>No todos!</p>
+        )}
+      </div>
+    </Context.Provider>
+  )
 }
 
-
-export default App;
+export default App
--- src/todo/Todolist.js
+++ src/todo/Todolist.js
@@ -1,28 +1,35 @@
 import React from 'react'
-import TodoItem from "./TodoItem";
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types'
+import TodoItem from './TodoItem'
 
-function TodoList (props){
-    const styles = {
-        ul: {
-            listStyle:'none',
-            margin:0,
-            padding:0,
-        }
-    }
+const styles = {
+  ul: {
+    listStyle: 'none',
+    margin: 0,
+    padding: 0
+  }
+}
 
-    return (
-        <div >
-            <ul style={styles.ul}>
-                {props.todos.map((todo,index)=>{return <TodoItem key={todo.id} todo={todo} index={index} onChange={props.onToggle}/>})}
-            </ul>
-        </div>
-    )
+function TodoList(props) {
+  return (
+    <ul style={styles.ul}>
+      {props.todos.map((todo, index) => {
+        return (
+          <TodoItem
+            todo={todo}
+            key={todo.id}
+            index={index}
+            onChange={props.onToggle}
+          />
+        )
+      })}
+    </ul>
+  )
 }
 
 TodoList.propTypes = {
-    todos: PropTypes.arrayOf(PropTypes.object).isRequired,
-    onToggle:PropTypes.func.isRequired
+  todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onToggle: PropTypes.func.isRequired
 }
 
 export default TodoList
